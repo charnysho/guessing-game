287a675b601510855311b7598242cb31c1a1ad0a
charnysho <charnysho@gmail.com>
2020-03-03 20:06:48 -0800

    add functionality to js file

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,25 @@
 'use strict'
 
-prompt('Is your name Olga?');
-prompt('Did you work as QA?');
-prompt('Do you have degree from Washinghtom university?');
-prompt('Do you want to be a painter?');
-prompt('Did you take CCNA Routing and Switching class?');git 
+var usersName = prompt('Hey there! What is your name?')
+print('Hello ' + usersName);
+
+var name = prompt('Is my name Olga?');
+var work = prompt('Did my work as QA?');
+var university = prompt('Do I have degree from Washinghtom university?');
+var job = prompt('Do I want to be a painter?');
+var passedClass = prompt('Did I take CCNA Routing and Switching class?'); 
+
+var yes = 'yes';
+var y = 'y';
+var no = 'no';
+var n = 'n';
+
+if(name.toLowerCase() == yes || name.toLowerCase() == y) {
+  console.log('Correct answer!');
+  alert('Correct answer!');
+} else {
+  console.log('Wrong answer!');
+  alert('Wrong answer!')
+}
+
+print('See you later ' + usersName);
