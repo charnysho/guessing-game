07d31197ca049d95cd34d9cb615a90cd3848aa2e
charnysho <charnysho@gmail.com>
2020-03-03 19:30:39 -0800

    Add HTML, JS and CSS files

---
+++ javaScript.js
@@ -0,0 +1,7 @@
+'use strict'
+
+prompt('Is your name Olga?');
+prompt('Did you work as QA?');
+prompt('Do you have degree from Washinghtom university?');
+prompt('Do you want to be a painter?');
+prompt('Did you take CCNA Routing and Switching class?');git 
