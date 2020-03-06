c69c211803a78cc527be4f468fbdb3dd7d92cb06
KentFletcher <kentfletch09@gmail.com>
2020-03-05 16:37:38 -0800

    functions

--- javaScript.js
+++ javaScript.js
@@ -1,38 +1,34 @@
 'use strict'
 
-var usersName = prompt('Hey there! What is your name?')
+
+var incorrectAnswerList = ['no', 'n'];
+var correctAnswerList = ['yes', 'y'];
+var correctNumber = getRandomInt(10);
+var favoriteFood = ['pasta', 'pizza', 'poke', 'sushi', 'seafood', 'meat', 'noodles', 'sandwich', 'soup', 'salad', 'snack'];
+var yesQuestions = ['Is my name Olga?', 'Did I work as QA?', 'Did I take CCNA Routing and Switching class?'];
+var noQuestions = ['Do I have degree from Washington university?', 'Do I want to be a painter?'];
+var totalCorrectAnswers = 0;
+
+
+var usersName = prompt('Hey there! What is your name?');
 alert('Hello there, ' + usersName + '!')
 
-var yes = 'yes';
-var y = 'y';
-var no = 'no';
-var n = 'n';
-var correctNumber = getRandomInt(10);
-var favouritefood = ['pasta', 'pizza', 'poke', 'suchi', 'seafood', 'meat', 'noodles', 'sandwich', 'soup', 'salad', 'snack'];
-var yesQuestions = ['Is my name Olga?', 'Did I work as QA?', 'Did I take CCNA Routing and Switching class?'];
-var noQuestions = ['Do I have degree from Washinghtom university?', 'Do I want to be a painter?'];
-var totalCorrectAnswers = null;
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-for (var i = 0; i < yesQuestions.length; i++) {
-  var answer = prompt(yesQuestions[i]);
-  quizz(answer, yes, y);
-}
-
-for (var i = 0; i < noQuestions.length; i++) {
-  var answer = prompt(noQuestions[i]);
-  quizz(answer, no, n);
-}
-
+askQuestion(yesQuestions, correctAnswerList);
+askQuestion(noQuestions, incorrectAnswerList);
 guessingNumberGame();
 favFoodGame();
 alert('Total correct answers: ' + totalCorrectAnswers);
+goodByeMessage(usersName);
 
-function quizz(answer, correctAnswer1, correctAnswer2) {
-  if(answer.toLowerCase() == correctAnswer1 || answer.toLowerCase() == correctAnswer2) {
+function askQuestion(questionType, correctAnswer) {
+  for (var i = 0; i < questionType.length; i++) {
+    var answer = prompt(questionType[i]);
+    quiz(answer, correctAnswer);
+  }
+}
+
+function quiz(answer, correctAnswerList) {
+  if(answer.toLowerCase() == correctAnswerList[0] || answer.toLowerCase() == correctAnswerList[1]) {
     console.log('Your answer is ' + answer + '. Correct!');
     alert('Correct answer!');
     totalCorrectAnswers = totalCorrectAnswers + 1
@@ -44,10 +40,10 @@ function quizz(answer, correctAnswer1, correctAnswer2) {
 }
 
 function favFoodGame() {
-  var possibleAnswers = ['pasta', 'suchi'];
+  var possibleAnswers = ['pasta', 'sushi'];
   var attemptNumber = 6;
   for(var i = 0; i < attemptNumber; i++) {
-    var answer6 = prompt('Please guess a food: ' + favouritefood + ' You have ' + attemptNumber + ' attempts');
+    var answer6 = prompt('Please guess a food: ' + favoriteFood + ' You have ' + attemptNumber + ' attempts');
     if(answer6 === possibleAnswers[0] || answer6 === possibleAnswers[1]) {
       alert('You are right!');
       totalCorrectAnswers = Number(totalCorrectAnswers) + 1
@@ -59,7 +55,6 @@ function favFoodGame() {
 
 function guessingNumberGame() {
   var attemptNumber = 4;
-  var a;
   for(var i = 0; i < attemptNumber; i++) {
     var answer6 = prompt('Please guess the number.');
     if(checkRightNumber(answer6)) {
@@ -86,4 +81,10 @@ function checkRightNumber(guessingNumber) {
   }
 }
 
-document.getElementById("seeYouLater").innerHTML = ('See you later, ' + usersName + '!').toUpperCase();
+function goodByeMessage(usersName) {
+  document.getElementById("seeYouLater").innerHTML = ('See you later, ' + usersName + '!').toUpperCase();
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
